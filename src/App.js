ff622fb0e55eeb897ccafdc3377414f57a7cb9d4
ru-ichikawa <ru-ichikawa@kentem.co.jp>
2022-09-27 14:10:09 +0900

    ログアウトテスト2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <a href="/.auth/me">My Info</a>
     </div>
     <div>
-      <a href="/.auth/logout?post_logout_redirect_uri=https://b2cdemo0915ri.b2clogin.com/b2cdemo0915ri.onmicrosoft.com/oauth2/v2.0/logout?p=B2C_1_swa_susi&post_logout_redirect_uri=https://proud-water-05bcc2d00.1.azurestaticapps.net/.auth/login/b2c/callback">Logout</a>
+      <a href="/.auth/logout?post_logout_redirect_uri=https://b2cdemo0915ri.b2clogin.com/b2cdemo0915ri.onmicrosoft.com/oauth2/v2.0/logout?p=B2C_1_swa_susi">Logout</a>
     </div>
   </div>;
 }
